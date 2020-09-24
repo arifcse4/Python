0d3395ed00062754ba88d0a65e92486699fc1cbb
ARIF <arif@kali.arif>
2020-09-24 01:23:48 -0400

    Working with a file content that add all lines together and show total digits.

--- file_reader.py
+++ file_reader.py
@@ -1,5 +1,8 @@
+pi_string = ''
 with open("pi_digits.txt") as f:
     lines = f.readlines()
     for line in lines:
-        print(line.strip())
-        
+        pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
