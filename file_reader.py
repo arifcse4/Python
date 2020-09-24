631381c0836e1867009444f40429c8f8889558da
ARIF <arif@kali.arif>
2020-09-24 03:01:06 -0400

    Added 1 million digits of pi in pi_digits.txt and check if a birthday sequence is exist or not through file_reader.py.

--- file_reader.py
+++ file_reader.py
@@ -1,8 +1,15 @@
 pi_string = ''
-with open("pi_digits.txt") as f:
-    lines = f.readlines()
-    for line in lines:
-        pi_string += line.strip()
 
-print(pi_string)
+with open("pi_digits.txt") as f:
+    for line in f.readlines():
+        pi_string += line
+
+#print(pi_string)
 print(len(pi_string))
+
+birthday = input("Enter Birthday format ddmmyy: ")
+
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday dose not apper in the first million digits of pi.")
