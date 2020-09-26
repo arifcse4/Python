9854c908b492d102f7417fcf2b73f9662b14ff9a
ARIF <arif@kali.arif>
2020-09-25 22:13:11 -0400

    Main code

--- file_reader.py
+++ file_reader.py
@@ -1,15 +1,19 @@
+file_name = 'pi_digits.txt'
+
+with open(file_name) as f:
+    lines = f.readlines()
+
 pi_string = ''
 
-with open("pi_digits.txt") as f:
-    for line in f.readlines():
-        pi_string += line
+for line in lines:
+    pi_string += line.strip()
 
-#print(pi_string)
 print(len(pi_string))
 
-birthday = input("Enter Birthday format ddmmyy: ")
+birthday = input("Enter Birthday(ddmmyy): ")
 
 if birthday in pi_string:
-    print("Your birthday appears in the first million digits of pi!")
+    print("Your birthday appears in the first million digits of pi!!!")
 else:
-    print("Your birthday dose not apper in the first million digits of pi.")
+    print("Your birthday does not appear in the first million digits fo pi.")
+
